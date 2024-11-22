9f973fdefd28c04ff614b19251c8964b9772aaaf
JoonYong <joonyongpark99@gmail.com>
2024-11-22 16:14:47 +0900

    [CONFIG] S3 설정 추가

---
+++ src/main/java/com/joon/fileupload/SpringFileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.joon.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileuploadApplication.class, args);
+	}
+
+}
