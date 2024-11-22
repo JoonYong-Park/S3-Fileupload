b26efdf017e8a2c3f349de55863673d6d7f5a0ba
JoonYong <joonyongpark99@gmail.com>
2024-11-22 16:09:08 +0900

    [INIT] Spring Boot project initial setup

---
+++ src/test/java/com/joon/fileupload/SpringFileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joon.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
